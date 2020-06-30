d930453d0335acca19f14b47e43f39ff315f2b5a
unknown <wangritian13@gmail.com>
2020-06-30 10:24:41 +0800

    change docker install

--- ubuntu_docker.sh
+++ ubuntu_docker.sh
@@ -9,9 +9,15 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-systemctl status docker
 
 sudo groupadd docker
 sudo gpasswd -a $USER docker
 newgrp docker
 docker ps
+
+echo "{" > /etc/docker/daemon.json
+echo "    \"registry-mirrors\": [\"https://registry.docker-cn.com\"]" >> /etc/docker/daemon.json
+echo "}" >> /etc/docker/daemon.json
+
+# test
+sudo docker run hello-world
